c67b35bfa193467a9a97d7acdf4d5aef3117b43f
Sudesh Gore <sudeshgore1112@gmail.com>
2025-07-02 20:46:37 +0530

    Solved first problem of day-1

--- Day 1-Javascript Basics/main.js
+++ Day 1-Javascript Basics/main.js
@@ -1,11 +1,4 @@
-var prompt = require('prompt-sync')();
-const age = prompt("Please enter your age to check the discount: ");
-if(age < 18){
-    console.log("You get a 20% discount!");
-}
-else if(age >= 18 && age <= 65){
-    console.log("Sorry, normal ticket price applied!");
-}
-else {
-    console.log("You get a 30% senior discount!");
-}
+var a = 5;
+var b = 6;
+var ans = a + b;
+console.log(ans);
