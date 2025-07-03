b354c5e114606e0c5053fb1f2bce1b77d0fde747
Sudesh Gore <sudeshgore1112@gmail.com>
2025-07-04 00:15:38 +0530

    Updated packages

---
+++ Day 2-JsCallbackFunction/notes.js
@@ -0,0 +1,12 @@
+console.log("This is notes file!");
+
+var age = 23;
+
+const addNumber = function (a,b){
+    return a+b;
+}
+
+module.exports = {
+    age, 
+    addNumber
+}
--- Day 2-JsCallbackFunction/server.js
+++ Day 2-JsCallbackFunction/server.js
@@ -14,15 +14,33 @@
 
 
 // # - FileSystem and Operating system libraries.
-var fs = require('fs');
-var os = require('os');
+// var fs = require('fs');
+// var os = require('os');
 
-var user = os.userInfo();
-console.log("User information: " , user);
-console.log("Username: " , user.username);
+// var user = os.userInfo();
+// console.log("User information: " , user);
+// console.log("Username: " , user.username);
 
-fs.appendFile('Greeting.txt',  'Hi, ' + user.username + '!\n' , ()=>{
-    console.log("File is created!");
-});
+// fs.appendFile('Greeting.txt',  'Hi, ' + user.username + '!\n' , ()=>{
+//     console.log("File is created!");
+// });
+
+// # Importing data from one file to another file
+// const notes = require('./notes.js');
+// console.log("Server file is available!");
+
+// var age = notes.age;
+// var result = notes.addNumber(age, 10);
+// console.log("Current age: ", age);
+// console.log("After 10 years age: ", result);
+
+// # Using lodash library.
+var _ = require('lodash');
+
+var data = ["person", "person", "Sudesh", "AIR-8", 8, 1, 8];
+var filter = _.uniq(data);
+console.log(filter);
+
+
 
 
