249348e16a998e8f8cf71c7867fc4cea16e1f6d9
Sudesh Gore <sudeshgore1112@gmail.com>
2025-07-04 23:19:26 +0530

    Complete exercise

---
+++ Day 1-Javascript Basics/Problem3.js
@@ -0,0 +1,21 @@
+const product1 = {
+    name: "Soap",
+    price: 30,
+    inStock: true
+};
+
+const product2 = {
+    name: "Shampoo",
+    price: 2,
+    inStock: true
+};
+
+const product3 = {
+    name: "Handwash",
+    price: 130,
+    inStock: false
+};
+
+console.log("Product 1 details: ", product1);
+console.log("Product 2 details: ", product2);
+console.log("Product 3 details: ", product3);
